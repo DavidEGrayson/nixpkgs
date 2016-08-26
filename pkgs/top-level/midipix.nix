924af3ca7a1d9fcd354973eaac9c9e1e4c12e6f2
David Grayson <davidegrayson@gmail.com>
2016-08-25 19:24:01 -0700

    midipix: Added a TODO.

--- pkgs/top-level/midipix.nix
+++ pkgs/top-level/midipix.nix
@@ -1,6 +1,14 @@
 # Hint: run
 #  nix-build pkgs/top-level/midipix.nix -A binutilsCross
 
+# TODO: what is wrong with this file?
+#
+# Why do these two commands give different results?
+# (e.g. different derivations, or the first one giving an error)
+#
+#  nix-instantiate pkgs/top-level/midipix.nix -A gcc
+#  nix-instantiate pkgs/top-level/impure.nix -A gcc
+
 import ./. {
   system = builtins.currentSystem;
   crossSystem = {
