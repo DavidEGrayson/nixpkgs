ef9129545bb120a0e8d920d9dce153d772e9b4f2
David Grayson <davidegrayson@gmail.com>
2016-09-06 19:40:49 -0700

    Added a TODO to buff the crossSystem tests.

--- test/cross_system.nix
+++ test/cross_system.nix
@@ -1,9 +1,9 @@
 # This file helps us test some assertions about the treatment of the
 # crossSystem top-level argument.
 
-# Assert that the presence of a crossSystem argument does not affect
-# native packages.  For now we just test a few packages that had
-# issues in the past, but maybe later we can test all of them.
+# Ideally, the presence of a crossSystem argument should not affect
+# *any* native derivations.  For now, we just test a few derivations
+# that are likely to have problems.
 rec {
   topLevel = import ../pkgs/top-level;
 
@@ -21,7 +21,14 @@ rec {
   };
 
   sameLibiconv = pkgs.libiconv == pkgsWithCross.libiconv;
+  sameGcc6 = pkgs.gcc6 == pkgsWithCross.gcc6;
   sameGcc5 = pkgs.gcc5 == pkgsWithCross.gcc5;
+  sameGcc49 = pkgs.gcc49 == pkgsWithCross.gcc49;
+  sameGcc48 = pkgs.gcc48 == pkgsWithCross.gcc48;
+  sameGcc45 = pkgs.gcc45 == pkgsWithCross.gcc45;
+
+  # TODO: Test that *all* GCCs are the same:
+  # sameGcc = sameGcc6 && sameGcc5 && sameGcc49 && sameGcc48 && sameGcc45;
 
   testsPass = sameLibiconv && sameGcc5;
 }
